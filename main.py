3a4128ae843d14fc904946d4f2f3ca4ba92c30a6
Tara Williams <taraewilliams12@gmail.com>
2017-11-08 14:13:16 -0500

    PCA and t-SNE finished.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import preprocess_data as preprocessor
 
 def main():
-    preprocessor.preprocess_data("Data", "CleanDataSmall", 0.5, 0.4, True)
+    preprocessor.preprocess_data("Data", "CleanDataSmall", 0.99, 0.02, True)
 
 main()

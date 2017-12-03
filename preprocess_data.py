e2d63940d07c6b63ddad1f18cde5d2a386d715f3
Tara Williams <taraewilliams12@gmail.com>
2017-12-03 16:25:18 -0500

    Added test grouped data when splitting the data set.

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from random import randint
 import os
+import math
 
 
-def preprocess_data(read_directory, write_directory, test_percentage, validation_percentage, convert_categorical=False, categorical_columns=["Weekday", "DepartmentDescription"], sort_label="TripType"):
+def preprocess_data(read_directory, write_directory, number_test_samples, sort_label="TripType", group_label="VisitNumber", convert_categorical=True, categorical_columns=["Weekday", "DepartmentDescription"]):
 
     # Make write directory if it does not exist
     if not os.path.isdir(write_directory):
@@ -18,26 +19,34 @@ def preprocess_data(read_directory, write_directory, test_percentage, validation
     data = clean_data(data, sort_label)
     write_data(write_directory + '/data.csv', data)
 
-    # Split data into test, training, and validation data
-    test_data, remaining_data = split_data(data, test_percentage, sort_label)
-    validation_data, training_data = split_data(remaining_data, validation_percentage, sort_label)
+    # Convert data to categorical data
+    if convert_categorical:
+        numerical_data = convert_categorical_columns(data, categorical_columns)
+        write_data(write_directory + '/data_numerical.csv', numerical_data)
+
+
+    # Get Grouped Test Data
+    data, groups = group_test_data(data, group_label, number_test_samples)
+    return data, groups
+
+
+def select_data_sets(data, write_directory, validation_percentage, convert_categorical=True, categorical_columns=["Weekday", "DepartmentDescription"], sort_label="TripType"):
+
+    # Split data into training and validation data
+    validation_data, training_data = split_data(data, validation_percentage, sort_label)
+
     # Write test, training, and validation data to CSV files
     write_data(write_directory + '/train.csv', training_data)
-    write_data(write_directory + '/test.csv', test_data)
     write_data(write_directory + '/validation.csv', validation_data)
 
     # Called if converting categories to numerical values
     if convert_categorical:
-        # Convert all data to numerical data
-        numerical_data = convert_categorical_columns(data, categorical_columns)
-        # Split data into test, training, and validation data
+        numerical_data = read_data(write_directory + "/data_numerical.csv")
+        # Split data into training and validation data
         training_data_numerical = numerical_data.loc[numerical_data.index.isin(training_data.index)]
-        test_data_numerical = numerical_data.loc[numerical_data.index.isin(test_data.index)]
         validation_data_numerical = numerical_data.loc[numerical_data.index.isin(validation_data.index)]
-        # Write all data, and test, training, and validation data to CSV files
-        write_data(write_directory + '/data_numerical.csv', numerical_data)
+        # Write training and validation data to CSV files
         write_data(write_directory + '/train_numerical.csv', training_data_numerical)
-        write_data(write_directory + '/test_numerical.csv', test_data_numerical)
         write_data(write_directory + '/validation_numerical.csv', validation_data_numerical)
 
 
@@ -62,6 +71,35 @@ def clean_data(data, order_label):
     return data
 
 
+# Group Test Data:
+#   Get test data that is grouped by a column value
+#   i.e. select random number of groups with shared column value VisitNumber
+def group_test_data(data, label, number_samples):
+    if (number_samples >= data.shape[0]):
+        number_samples = data.shape[0]
+
+    visits = data[label].unique().tolist()
+
+    groups = []
+    group_indices = []
+    group_values = []
+
+    while len(groups) < number_samples:
+        visit_index = randint(0, len(visits))
+
+        if (visit_index not in group_indices):
+            visit = visits[visit_index]
+
+            group_indices.append(visit_index)
+            group_values.append(visit)
+
+            group = data.loc[data[label]==visit]
+            groups.append(group.values)
+
+    data = data[~data[label].isin(group_values)]
+    return data, groups
+
+
 # Convert categorical columns to numerical columns
 def convert_categorical_columns(data, category_labels):
     numerical_data = convert_categorical_to_numerical(data.copy(), category_labels)
@@ -80,8 +118,10 @@ def split_data(data, train_percentage, label):
     for trip in trip_types:
         group = data.loc[data[label]==trip]
 
+        number_samples = math.floor((train_percentage * group.shape[0]) / 100)
+
         # Choose training data
-        training_sample = group.sample(frac=train_percentage)
+        training_sample = group.sample(n=number_samples)
         training_data = training_data.append(training_sample, ignore_index=True)
         # Choose test data
         test_sample = group.loc[~group.index.isin(training_sample.index)]
